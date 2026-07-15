a8b05d3ae4d01f93acdec3f863f4b8671f3b6305
dylanesneideralarconbarrero-eng <dylanesneideralarconbarrero@gmail.com>
2026-07-15 15:09:52 -0500

    Actualización del proyecto

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+function DashboardPage() {
+  return (
+    <>
+      <h1>hola desde dashboard</h1>
+      
+    </>
+  );
+}
+
+export default DashboardPage
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import NaBar from "@/components/NaBar";
+export default function Home() {
+  return (
+    <>
+      <NaBar />
+    </>
+  );
+}
